5a80d1f05af8dca184311d5565d9693010ca8099
Цветков Анатолий Сергеевич <tsvetkov@mkb.ru>
2025-01-27 17:50:34 +0300

    Add check element

--- src/test/java/TestForms.java
+++ src/test/java/TestForms.java
@@ -54,6 +54,7 @@ public class TestForms {
         $("#permanentAddress").setValue("street 2");
         $("#submit").click();
         //assertTrue($(".field-error").isDisplayed());
+        $(".field-error").should(appear);
     }
 
     @AfterEach
